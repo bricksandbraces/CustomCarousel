033b411b0db68261ba08d999ba2c86d4b6779fe4
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2022-04-27 20:39:41 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomCarousel
+
+A description of this package.
---
+++ Sources/CustomCarousel/CustomCarousel.swift
@@ -0,0 +1,6 @@
+public struct CustomCarousel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
